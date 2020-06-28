65a46a8c5d3bbb4eaccfc8366d578faf83a7c023
@mohdarif8299 <mohdarif8299@gmail.com>
2020-06-28 11:45:18 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private httpClient:HttpClient) {
+  }
+    selectedFile: File;
+    retrievedImage: any;
+    message: string;
+    error:string;
+    isUploading:string;
+    imageName: any;
+    
+    //Gets called when the user selects an image
+    public onFileChanged(event) {
+      //Select File
+      this.selectedFile = event.target.files[0];
+    }
+    //Gets called when the user clicks on submit to upload the image
+    onUpload() {
+      console.log(this.selectedFile);
+      //FormData API provides methods and properties to allow us easily prepare form data to be sent with POST HTTP requests.
+      this.isUploading = "true";
+      const uploadImageData = new FormData();
+      uploadImageData.append('imageFile', this.selectedFile, this.selectedFile.name);
+      //Make a call to the Spring Boot Application to save the image
+      this.httpClient.post('http://localhost:8080/image/upload', uploadImageData, {responseType: 'text'})
+        .subscribe((response) => {
+          if (response === 'success') {
+            this.isUploading = '';
+            this.message = 'Image uploaded successfully';
+          } else {
+            this.error = 'Image not uploaded successfully';
+          }
+        }
+        );
+    }
+      //Gets called when the user clicks on retieve image button to get the image from back end
+      getImage() {
+      //Make a call to Sprinf Boot to get the Image Bytes.
+      this.httpClient.get('http://localhost:8080/image/get/' + this.imageName,{responseType: 'text'})
+        .subscribe(
+          res => {
+            console.log(res);
+          //  let response = JSON.parse(res);
+       //     console.log(res)
+         //  let imageURL = window.URL.createObjectURL(res);
+       //this.retrievedImage = this.sanitizer.bypassSecurityTrustUrl(imageURL);
+     //  console.log(JSON.parse(res.toString()));
+     this.retrievedImage= res;
+     // this.base64Data = this.retrieveResonse.picByte;
+         //   this.retrievedImage = 'data:image/jpeg;base64,' + res;
+            //  let imageURL = 'data:image/jpg' + res;
+            //  this.retrievedImage = this.sanitizer.bypassSecurityTrustUrl(imageURL);
+
+          }
+        ); 
+    }
   title = 'FileUploadApp';
 }
